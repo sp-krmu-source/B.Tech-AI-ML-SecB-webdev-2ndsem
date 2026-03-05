a9574160ed3ffa7bb4109e3159aba77c1b771d59
S.P Acharya <spacharya@SPs-MacBook-Air.local>
2026-03-05 12:09:16 +0530

    promise , guess the output

--- JS-Day-10/script.js
+++ JS-Day-10/script.js
@@ -24,15 +24,65 @@ setTimeout(()=>{
 },1000)
 
 //promises
-let pro = new Promise((resolve,reject)=>{
-    let proposal="false"
-    if(proposal==="true"){
-        resolve()
-    }
-    else{
-        reject()
-    }
-})
-pro.then(()=>console.log("proposal accepted"))
-.catch(()=>console.log("proposal rejected"))
+
+// if promise fullfilled then it will go to .then() method.
+// if promise is rejected then it will go to .catch() method
+
+
+
+// let pro = new Promise((resolve,reject)=>{
+//     let proposal="false"
+//     if(proposal==="true"){
+//         resolve()
+//     }
+//     else{
+//         reject()
+//     }
+// })
+// pro.then(()=>console.log("proposal accepted"))
+// .catch(()=>console.log("proposal rejected"))
+
+// same code with function syntax
+
+// function pro(){
+//  return new Promise((resolve,reject)=>{
+//     let proposal="false"
+//     if(proposal==="false"){
+//         resolve()
+//     }
+//     else{
+//         reject()
+//     }
+// })
+// }
+// pro().then(()=>console.log("proposal accepted"))
+// .catch(()=>console.log("proposal rejected"))
+
+
+// Event loop:  it is a constantly running process
+
+
+
+// Interview Trap Question: find the reason behind it
+setTimeout(()=>{
+    console.log("setTimeout 1")
+},0)
+    
+function delay() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      console.log("Timeout Done");
+      resolve();
+    }, 0);
+  });
+}
+
+function test() {
+  console.log("Start");
+  delay();
+  console.log("After Delay");
+}
+
+test();
+console.log("End");
 
